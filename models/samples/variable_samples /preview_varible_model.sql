64f02b1b613bda7f15d2684ddfe5db3790238130
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-01-22 18:31:55 +0000

    updates

---
+++ models/samples/variable_samples /preview_varible_model.sql
@@ -0,0 +1,10 @@
+{{
+    config(
+        materialized='insert_into'
+    )
+}}
+Select
+
+*
+
+from {{ ref('sample_varible_model') }}
---
+++ models/samples/variable_samples /view_preview_model.sql
@@ -0,0 +1,5 @@
+Select
+
+*
+
+from {{ ref('preview_varible_model') }}
