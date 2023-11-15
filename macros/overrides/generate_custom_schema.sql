324c53fa72f32bc8a4817fbddb365048f358ee9d
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-11-15 23:21:54 +0000

    updates

--- macros/overrides/generate_custom_schema.sql
+++ macros/overrides/generate_custom_schema.sql
@@ -1,13 +1,30 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
 
-        default_schema
+    {% if env_var("DBT_ENV_NAME") == 'DEV' %}
 
-    {%- else -%}
+        {%- if custom_schema_name is none -%}
 
-        {{ custom_schema_name | trim }}
+            {{ 'DEV__' ~ default_schema }}
+
+        {%- else  -%}
+
+             {{ 'DEV__' ~ custom_schema_name | trim }}
+
+        {%- endif -%}
+
+    {% elif env_var("DBT_ENV_NAME") == 'PRD' %}
+
+        {%- if custom_schema_name is none -%}
+
+            {{ 'PRD__' ~ default_schema }}
+
+        {%- else  -%}
+
+            {{'PRD__' ~  custom_schema_name | trim }}
+
+        {%- endif -%}
 
     {%- endif -%}
 
--- tests/stg_tpch_orders_assert_positive_price.sql
+++ tests/stg_tpch_orders_assert_positive_price.sql
@@ -1,4 +1,14 @@
-select 
-	*
-from {{ ref('stg_tpch_orders') }}
-	where total_price < 0
+with base as (
+  select *
+  from {{ ref('stg_tpch_orders') }}
+)
+
+, sampled as (
+  select *
+  from base
+  where rand() < 0.01 -- Adjust the sampling rate as needed
+)
+
+select *
+from sampled
+where total_price < 0
