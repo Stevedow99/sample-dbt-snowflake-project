ea410a5eecdd27ea8787234de7dc2b7bd15e663f
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-11-17 20:23:12 +0000

    updates

--- macros/overrides/generate_custom_schema.sql
+++ macros/overrides/generate_custom_schema.sql
@@ -1,13 +1,24 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
 
-        default_schema
+    {% if env_var("DBT_ENV_NAME") == 'DEV' %}
 
-    {%- else -%}
 
-        {{ custom_schema_name | trim }}
+            {{ 'DEV__' ~ default_schema }}
+
+
+    {% elif env_var("DBT_ENV_NAME") == 'PRD' %}
+
+        {%- if custom_schema_name is none -%}
+
+            {{ 'PRD__' ~ default_schema }}
+
+        {%- else  -%}
+
+            {{'PRD__' ~  custom_schema_name | trim }}
+
+        {%- endif -%}
 
     {%- endif -%}
 
--- models/marts/aggregates/monthly_gross_revenue.sql
+++ models/marts/aggregates/monthly_gross_revenue.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        schema='revenue'
+    )
+}}
+
 select
     date_trunc(month, order_date) as order_month,
     sum(gross_item_sales_amount) as gross_revenue
