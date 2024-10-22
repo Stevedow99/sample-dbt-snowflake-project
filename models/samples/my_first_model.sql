60770a750cd5623c8a9e196253efba58369135c9
Steve Dowling <steve.dowling@fishtownanalytics.com>
2024-10-22 19:59:26 +0000

    updates

--- models/samples/my_first_model.sql
+++ models/samples/my_first_model.sql
@@ -10,4 +10,5 @@
 Select 
     'this is great column!' as my_frist_column,
     1/1234 as a_simple_calculation,
-    current_timestamp() as current_time
+    current_timestamp() as current_time,
+    '{{ target.name }}' as target_name
